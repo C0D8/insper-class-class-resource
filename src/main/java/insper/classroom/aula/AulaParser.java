31f465ebde0a931620cb88981abb2554427975eb
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-04-04 17:22:32 -0300

    att aula parser

--- src/main/java/insper/classroom/aula/AulaParser.java
+++ src/main/java/insper/classroom/aula/AulaParser.java
@@ -6,18 +6,24 @@ public class AulaParser {
 
     public static Aula to(CreateAulaIn in) {
         return Aula.builder()
-                .email(in.email())
-                .name(in.name())
-                .password(in.password())
+                .id_professor(in.id_professor())
+                .nome(in.nome())
+                .descricao(in.descricao())
+                .data(in.data())
+                .duracao(in.duracao())
                 .build();
     }
-
-    public static CreateAulaOut to(Aula account) {
+    
+    public static CreateAulaOut to(Aula aula) {
         return CreateAulaOut.builder()
-                .id(account.id())
-                .email(account.email())
-                .name(account.name())
+                .id(aula.id())
+                .id_professor(aula.id_professor())
+                .nome(aula.nome())
+                .descricao(aula.descricao())
+                .data(aula.data())
+                .duracao(aula.duracao())
                 .build();
+
     }
 
 }
